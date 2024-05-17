cc7954e25005c3b5f23aabcdd187418ee4c0cef1
lvsouza <ls18017@gmail.com>
2024-05-17 19:18:15 -0300

    feat: add button for section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function PageHome() {
           items={[
             {
               href: '/cursos/123',
-              title: 'Curso de API Rest, Node e Typescript',
+              title: '1 Curso de API Rest, Node e Typescript',
               image: 'https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg',
               description: `
               Na primeira aula do curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.
@@ -72,7 +72,7 @@ export default function PageHome() {
             },
             {
               href: '/cursos/123',
-              title: 'Curso de API Rest, Node e Typescript',
+              title: '2 Curso de API Rest, Node e Typescript',
               image: 'https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg',
               description: `
               Na primeira aula do curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.
@@ -127,7 +127,7 @@ export default function PageHome() {
             },
             {
               href: '/cursos/123',
-              title: 'Curso de API Rest, Node e Typescript',
+              title: '3 Curso de API Rest, Node e Typescript',
               image: 'https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg',
               description: `
               Na primeira aula do curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.
@@ -182,7 +182,7 @@ export default function PageHome() {
             },
             {
               href: '/cursos/123',
-              title: 'Curso de API Rest, Node e Typescript',
+              title: '4 Curso de API Rest, Node e Typescript',
               image: 'https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg',
               description: `
               Na primeira aula do curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.
--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,3 +1,7 @@
+'use client';
+import { useRef } from 'react';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+
 import { Card, ICardProps } from '../card/Card';
 
 
@@ -7,6 +11,15 @@ interface ISectionProps {
   variant: 'grid' | 'h-list';
 }
 export const Section = ({ title, items, variant = 'grid' }: ISectionProps) => {
+  const scrollRef = useRef<HTMLUListElement>(null);
+
+
+  const handleScroll = (scroll: number) => {
+    const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+    scrollRef.current?.scrollTo({ behavior: 'smooth', left: currentScrollLeft + scroll });
+  };
+
+
   return (
     <section className='flex flex-col gap-4 px-4'>
       <h2 className='font-bold text-xl'>
@@ -14,9 +27,16 @@ export const Section = ({ title, items, variant = 'grid' }: ISectionProps) => {
       </h2>
 
       <ul
+        ref={scrollRef}
         data-variant={variant}
         className='grid gap-2 grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto'
       >
+        <button
+          onClick={() => handleScroll(-350)}
+          className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto left-0 -ml-14'
+        >
+          <MdKeyboardArrowLeft size={32} />
+        </button>
 
         {items.map(item => (
           <li key={item.title} data-variant={variant} className='w-full data-[variant=h-list]:sm:w-72'>
@@ -29,6 +49,12 @@ export const Section = ({ title, items, variant = 'grid' }: ISectionProps) => {
           </li>
         ))}
 
+        <button
+          onClick={() => handleScroll(350)}
+          className='h-14 w-14 bg-primary rounded-full flex items-center justify-center sticky my-auto right-0 -ml-14'
+        >
+          <MdKeyboardArrowRight size={32} />
+        </button>
       </ul>
     </section>
   );
