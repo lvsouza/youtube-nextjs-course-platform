076023541dce482473672af16131ca5e64b19c9a
lvsouza <ls18017@gmail.com>
2024-08-15 17:47:45 -0300

    feat: fix padding bottom and course header props

--- src/components/course-header/CourseHeader.tsx
+++ src/components/course-header/CourseHeader.tsx
@@ -1,24 +1,30 @@
+'use client';
 import { MdShare } from 'react-icons/md';
 
 import { CollapsibleText } from './components/CollapsibleText';
 import { ContentCopy } from './components/CopyContext';
 
 
-export const CourseHeader = () => {
+interface ICourseHeaderProps {
+  title: string;
+  description: string;
+  numberOfClasses: number;
+}
+export const CourseHeader = ({ title, description, numberOfClasses }: ICourseHeaderProps) => {
 
 
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='font-extrabold text-xl'>
-        🎩 Curso de Figma para DEVs
+        {title}
       </h1>
 
       <CollapsibleText numberOfLinesWhenClosed={3}>
-        Os melhores desenvolvedores do mercado fazem questão que estar preparados para os mais diversos tipos de desafios nas suas carreiras. A habilidade de desenvolver protótipos ou mesmo de entender como um protótipo foi desenvolvido pode ser um baita diferencial para você. Nesse curso que te mostrar de forma simples e prática como desenvolver protótipos no figma, vamos aproveitar certos conhecimentos de programação ao decorrer do curso. Tenho certeza que esse tem o potencial de ser o melhor curso de figma para desenvolvedores disponíveis gratuitamente. #CODARSE
+        {description}
       </CollapsibleText>
 
       <div className='flex gap-2 items-center'>
-        <ContentCopy title='Copie o link abaixo' content='https://codarse.com/cursos/PL29TaWXah3iZqMoLjUU8EoKXQXjuc50F1'>
+        <ContentCopy title='Copie o link abaixo' content={window.location.href}>
           <button className='py-2 px-4 bg-paper rounded-full flex gap-2 items-center'>
             <MdShare />
             Compartilhar
@@ -26,7 +32,7 @@ export const CourseHeader = () => {
         </ContentCopy>
 
         <span>
-          48 aulas
+          {numberOfClasses} aulas
         </span>
       </div>
     </div>
