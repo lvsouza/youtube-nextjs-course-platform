c74f9840c7c7f089d5b11d27023d01b39f83b926
lvsouza <ls18017@gmail.com>
2024-09-10 19:16:32 -0300

    feat: add tabs components

--- src/components/player/player-class-details/PlayerClassDetails.tsx
+++ src/components/player/player-class-details/PlayerClassDetails.tsx
@@ -1,9 +1,10 @@
 'use client';
+import { useMemo } from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
 import { useRouter } from 'next/navigation';
 
 import { IPlayerClassGroupProps } from '../playlist/components/PlayerClassGroup';
 import { PlayerVideoPlayer } from './components/PlayerVideoPlayer';
-import { useMemo } from 'react';
 
 
 interface IPlayerClassDetailsProps {
@@ -39,9 +40,40 @@ export const PlayerClassDetails = ({ playingCourseId, playingClassId, classGroup
         />
       </div>
 
-      <div>
-        Descrição
-      </div>
+      <Tabs.Root defaultValue='class-details'>
+        <Tabs.List className='flex gap-4'>
+          <Tabs.Trigger
+            value='class-details'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Visão geral
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value='class-comments'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Comentários
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            value='course-details'
+            className='p-2 flex items-center justify-center border-b-4 border-transparent data-[state=active]:border-primary'
+          >
+            Visão geral do curso
+          </Tabs.Trigger>
+        </Tabs.List>
+
+        <hr className='border-paper' />
+
+        <Tabs.Content value='class-details'>
+          Detalhes da aula
+        </Tabs.Content>
+        <Tabs.Content value='class-comments'>
+          Comentários da aula
+        </Tabs.Content>
+        <Tabs.Content value='course-details'>
+          Detalhes do curso
+        </Tabs.Content>
+      </Tabs.Root>
     </div>
   );
 };
